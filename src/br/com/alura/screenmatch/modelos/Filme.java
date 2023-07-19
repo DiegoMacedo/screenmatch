39f3c37795f79872dd9dc211f22c366f4830fd21
DiegoMacedo <firewallsist@gmail.com>
2023-07-18 21:19:00 -0400

    segundo commit

--- src/br/com/alura/screenmatch/modelos/Filme.java
+++ src/br/com/alura/screenmatch/modelos/Filme.java
@@ -1,16 +1,49 @@
 package br.com.alura.screenmatch.modelos;
 
 public class Filme {
-    public String nome;
-    public int anoDeLancamento;
-    public boolean incluindoNoPlano;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluindoNoPlano;
     private double somaDasAvaliacoes;
     private int totalDeAvaliacoes;
-    public int duracaoEmMinutos;
+    private int duracaoEmMinutos;
 
     public int getTotalDeAvaliacoes(){
         return totalDeAvaliacoes;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluindoNoPlano() {
+        return incluindoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluindoNoPlano(boolean incluindoNoPlano) {
+        this.incluindoNoPlano = incluindoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
     public void exibeFichaTecnica(){
         System.out.println("Nome do br.com.alura.screenmatch.modelos.Filme: " + nome);
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
